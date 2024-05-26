1d7f5a91e68264dc66456549daa00350c5c5bfd4
nazish shaheen <156086159+nazishshaheen@users.noreply.github.com>
2024-05-26 09:28:21 +0000

    added a question array

---
+++ questions.js
@@ -0,0 +1,40 @@
+let questionArray = [
+    {
+        question: "Which is the smallest continent in the world?",
+        answer:[
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "America", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest animal in the world?",
+        answer:[
+            { text: "Shark", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Lion", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answer:[
+            { text: "Vatican city", correct: true},
+            { text: "Nepal", correct: false},
+            { text: "India", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest dessert in the world?",
+        answer:[
+            { text: "Sahara", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Kalahari", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    },
+]
+
+export default questionArray;
