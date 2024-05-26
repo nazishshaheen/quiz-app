1a8c06ef8b50b0e11bd0038fa06b93d16a2dda72
nazish shaheen <156086159+nazishshaheen@users.noreply.github.com>
2024-05-26 12:28:37 +0000

    added functionality for next question and many more

--- questionbody.js
+++ questionbody.js
@@ -1,19 +1,36 @@
 import questionArray from "./questions.js";
-import { questionElement, answerBtn, nextBtn } from "./quizapp.js";
+import { questionElement, answerBtn, nextBtn, showAnswer, nextQuestionEvent, incrementQuestionIndex } from "./quizapp.js";
+import { hideElement, showElement} from "./functions.js";
+import { checkAnswer, showResult } from "./evaluate.js";
 
 function nextQuestion(prevQuestion) {
+    answerBtn.innerHTML = null;
+    if (prevQuestion < questionArray.length - 2) {
+        hideElement(nextBtn);
+    }else{
+        showElement(nextBtn);
+    }
+    showElement(document.getElementById('skip-btn'));
     if (prevQuestion + 1 < questionArray.length) {
-        if (prevQuestion === questionArray.length - 1) {
-            nextBtn.disabled = true;
+        console.log(prevQuestion, questionArray.length);
+        if (prevQuestion === questionArray.length - 2) {
+            nextBtn.innerText = 'Submit';
+            document.getElementById('skip-btn').style.display = 'none';
+            nextBtn.removeEventListener('click', nextQuestionEvent);
+            nextBtn.addEventListener('click', () => {
+                showResult();
+            });
         }
         let currentQuestion = questionArray[prevQuestion + 1]
-        questionElement.innerHTML = currentQuestion.question;
-    } else {
-        alert('last question');
+        let questionIndex = prevQuestion + 1;
+        addQAnswers(currentQuestion, questionIndex);
     }
+    // incrementQuestionIndex();
 }
 
-function addAnswers(currentQuestion) {
+function addQAnswers(currentQuestion, questionIndex) {
+    let questionNo = questionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
     currentQuestion.answer.forEach(answer => {
         let button = document.createElement('button');
         button.innerHTML = answer.text;
@@ -22,11 +39,18 @@ function addAnswers(currentQuestion) {
 
         if (answer.correct) {
             button.dataset.correct = answer.correct;
+        } else {
+            button.dataset.correct = answer.correct;
         }
-        button.addEventListener('click', selectAnswer);
+        button.addEventListener('click', (e) => {
+            showAnswer(e.target.parentElement);
+            checkAnswer(e);
+            hideElement(document.getElementById('skip-btn'))
+        });
     })
 }
 
-export{
-    addAnswers
+export {
+    addQAnswers,
+    nextQuestion
 }
